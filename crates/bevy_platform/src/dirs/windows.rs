targrub/bevy

commit df306bb6bca68ee033c81826d72a1bc40a1f3778

Add `dirs` module to bevy_platform (#22891)
# Objective

Adds a `dirs` module which provides the location of the application
preferences directory.

Part of: #13311

crates/bevy_platform/src/dirs/windows.rs
@@ -1,0 +1,33 @@
+extern crate windows_sys as windows;
+use std::ffi::{c_void, OsString};
+use std::os::windows::ffi::OsStringExt;
+use std::path::PathBuf;
+use std::slice;
+use windows::Win32::UI::Shell;
+
+/// Returns the path to the directory used for application settings.
+// From https://github.com/dirs-dev/dirs-sys-rs/blob/main/src/lib.rs
+#[allow(unsafe_code)]
+fn known_folder(folder_id: windows::core::GUID) -> Option<PathBuf> {
+    // SAFETY: SHGetKnownFolderPath allocates path_ptr which must be freed with CoTaskMemFree.
+    unsafe {
+        let mut path_ptr: windows::core::PWSTR = std::ptr::null_mut();
+        let result =
+            Shell::SHGetKnownFolderPath(&folder_id, 0, std::ptr::null_mut(), &mut path_ptr);
+        if result == 0 {
+            let len = windows::Win32::Globalization::lstrlenW(path_ptr) as usize;
+            let path = slice::from_raw_parts(path_ptr, len);
+            let ostr: OsString = OsStringExt::from_wide(path);
+            windows::Win32::System::Com::CoTaskMemFree(path_ptr as *const c_void);
+            Some(PathBuf::from(ostr))
+        } else {
+            windows::Win32::System::Com::CoTaskMemFree(path_ptr as *const c_void);
+            None
+        }
+    }
+}
+
+/// Returns the path to the directory used for application settings.
+pub fn preferences_dir() -> Option<PathBuf> {
+    known_folder(Shell::FOLDERID_LocalAppData)
+}

crates/bevy_platform/Cargo.toml
@@ -97,6 +97,14 @@ spin = { version = "0.10.0", default-features = false, features = [
 [target.'cfg(not(target_has_atomic = "ptr"))'.dependencies]
 portable-atomic-util = { version = "0.2.4", default-features = false }
 
+[target.'cfg(windows)'.dependencies]
+windows-sys = { version = ">= 0.59.0", features = [
+  "Win32_UI_Shell",
+  "Win32_Foundation",
+  "Win32_Globalization",
+  "Win32_System_Com",
+] }
+
 [lints]
 workspace = true
 

crates/bevy_platform/src/dirs/linux.rs
@@ -1,0 +1,7 @@
+use std::{env::home_dir, path::PathBuf};
+
+/// Returns the path to the directory used for application settings.
+pub fn preferences_dir() -> Option<PathBuf> {
+    // TODO: Support XDG_CONFIG
+    home_dir().map(|home| home.join(".config"))
+}

crates/bevy_platform/src/dirs/macos.rs
@@ -1,0 +1,6 @@
+use std::{env::home_dir, path::PathBuf};
+
+/// Returns the path to the directory used for application settings.
+pub fn preferences_dir() -> Option<PathBuf> {
+    home_dir().map(|home| home.join("Library/Preferences"))
+}

crates/bevy_platform/src/dirs/mod.rs
@@ -1,0 +1,28 @@
+//! APIs that return the location of standard user directories.
+
+// Modeled after https://github.com/dirs-dev/dirs-sys-rs/
+
+#[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+use std::path::PathBuf;
+
+#[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "windows")]
+pub use windows::preferences_dir;
+
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+pub use macos::preferences_dir;
+
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+pub use linux::preferences_dir;
+
+/// Returns the path to the directory used for application settings. This version
+/// always returns `None`.
+#[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+pub fn preferences_dir() -> Option<PathBuf> {
+    None
+}

crates/bevy_platform/src/lib.rs
@@ -11,6 +11,8 @@
 
 cfg::std! {
     extern crate std;
+
+    pub mod dirs;
 }
 
 cfg::alloc! {

commit 14016f3b331a22dc86cf83ce931f878c6e87079b

Fix clippy issues with `bevy_platform::dirs` on Windows (#22926)
Fixes clippy issues that were introduced by #22891 and only occur on
Windows.

Tested on Win10 by hacking the `hello_world` example to print
`preferences_dir()`.

crates/bevy_platform/src/dirs/windows.rs
@@ -1,19 +1,20 @@
 extern crate windows_sys as windows;
-use std::ffi::{c_void, OsString};
+use alloc::slice;
+use core::ffi::c_void;
+use std::ffi::OsString;
 use std::os::windows::ffi::OsStringExt;
 use std::path::PathBuf;
-use std::slice;
 use windows::Win32::UI::Shell;
 
 /// Returns the path to the directory used for application settings.
 // From https://github.com/dirs-dev/dirs-sys-rs/blob/main/src/lib.rs
-#[allow(unsafe_code)]
+#[expect(unsafe_code, reason = "Uses unsafe Windows API functions")]
 fn known_folder(folder_id: windows::core::GUID) -> Option<PathBuf> {
     // SAFETY: SHGetKnownFolderPath allocates path_ptr which must be freed with CoTaskMemFree.
     unsafe {
-        let mut path_ptr: windows::core::PWSTR = std::ptr::null_mut();
+        let mut path_ptr: windows::core::PWSTR = core::ptr::null_mut();
         let result =
-            Shell::SHGetKnownFolderPath(&folder_id, 0, std::ptr::null_mut(), &mut path_ptr);
+            Shell::SHGetKnownFolderPath(&folder_id, 0, core::ptr::null_mut(), &mut path_ptr);
         if result == 0 {
             let len = windows::Win32::Globalization::lstrlenW(path_ptr) as usize;
             let path = slice::from_raw_parts(path_ptr, len);